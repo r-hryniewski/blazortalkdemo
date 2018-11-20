c1f84422ab44618862dfdd280adeaf6072d9b11c
Rafal Hryniewski <rafal@hryniewski.net>
2018-11-20 23:22:20 +0100

    Fixes

---
+++ blazortalkdemo/blazortalkdemo/InvokableFromJS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.JSInterop;
+
+namespace blazortalkdemo
+{
+    public static class InvokableFromJS
+    {
+        [JSInvokable(nameof(ReturnRandomGuid))]
+        public static string ReturnRandomGuid()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
